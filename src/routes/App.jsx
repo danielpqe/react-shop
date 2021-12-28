f6151d8a942607924a9744ae2ff4318ce8277f86
Daniel Quispe (Aegis Company) <v-daquis@microsoft.com>
2021-12-28 15:11:53 -0500

    creating schema, architecture

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../styles/global.css";
 import Layout from "../containers/Layout.jsx";
-import Login from "../containers/Login.jsx";
+import Login from "../pages/Login.jsx";
 import RecoveryPassword from "../containers/RecoveryPassword.jsx";
 import Home from "../pages/Home.jsx";
 import NotFound from "../pages/NotFound.jsx";
+import SendEmail from "../pages/SendEmail.jsx";
 
 const App = () => {
   return (
@@ -18,6 +19,12 @@ const App = () => {
             path="/recovery-password"
             element={<RecoveryPassword />}
           />
+          <Route path="/send-email" element={<SendEmail />} />
+          {/* <Route path="/new-password" component={<NewPassword />} />
+          <Route path="/account" component={<MyAccount />} />
+          <Route path="/signup" component={<CreateAccount />} />
+          <Route path="/checkout" component={<Checkout />} />
+          <Route path="/orders" component={<Orders />} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
