aacc851c7088f5ad108bfa3d6363be2b00dbb9ba
danielpqe <danielpqe@gmail.com>
2022-01-05 09:56:33 -0500

    implementing react context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import '../styles/Header.scss';
+import React, { useState, useContext } from 'react';
+import '@styles/Header.scss';
+import menu from '@icons/icon_menu.svg'
+import logo from '@logos/logo_yard_sale.svg'
+import shoppingCart from '@icons/icon_shopping_cart.svg'
+import Menu from '@components/Menu.jsx'
+import AppContext from '../context/AppContext';
+import MyOrder from '../containers/MyOrder'
 
 const Header = () => {
+	const [toggle, setToggle] = useState(false)
+	const [toggleOrders, setToggleOrders] = useState(false)
+	const { state } = useContext(AppContext)
+	const handleToggle = () => {
+		setToggle(!toggle)
+	}
 	return (
 		<nav>
-			<img src="./icons/icon_menu.svg" alt="menu" className="menu" />
+			<img src={menu} alt="menu" className="menu" />
 			<div className="navbar-left">
-				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+				<img src={logo} alt="logo" className="nav-logo" />
 				<ul>
 					<li>
 						<a href="/">All</a>
@@ -30,13 +42,18 @@ const Header = () => {
 			</div>
 			<div className="navbar-right">
 				<ul>
-					<li className="navbar-email">platzi@example.com</li>
-					<li className="navbar-shopping-cart">
-						<img src="./icons/icon_shopping_cart.svg" alt="shopping cart" />
-						<div>2</div>
+					<li className="navbar-email"
+						onClick={handleToggle}>
+						danielpqe@gmail.com
+					</li>
+					<li className="navbar-shopping-cart" onClick={()=>setToggleOrders(!toggleOrders)}>
+						<img src={shoppingCart} alt="shopping cart" />
+						<div>{state.cart.length > 0 ? state.cart.length : 0}</div>
 					</li>
 				</ul>
 			</div>
+			{toggle && <Menu />}
+			{toggleOrders && <MyOrder />}
 		</nav>
 	);
 }
